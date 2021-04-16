907d72e0d63648ccda31875062075969148927e6
David Kanenwisher <d.kanen@gmail.com>
2021-04-16 12:16:08 -0500

    Initial Commit

---
+++ Shared/DateTimeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateTimeCalculatorApp.swift
+//  Shared
+//
+//  Created by David Kanenwisher on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct DateTimeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
